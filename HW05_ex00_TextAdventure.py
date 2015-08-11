d2aa3fd27b0c628f24fe9815a48322b5d7550339
Anne Jonas <ajonas@ischool.berkeley.edu>
2015-08-10 17:37:46 -0700

    TextAdventure game without enc ftm

--- HW05_ex00_TextAdventure.py
+++ HW05_ex00_TextAdventure.py
@@ -2,75 +2,72 @@
 # HW05_ex00_TextAdventure.py
 ##############################################################################
 # Imports
-from sys import exit
+import sys
 
 # Body
 
-
-def infinite_stairway_room(count=0):
-    print "You walk through the door to see a dimly lit hallway."
-    print "At the end of the hallway is a", count * 'long ', 'staircase going towards some light'
+def infinite_stairway_room(user, count=0):
+    # print user + " walks through the door to see a dimly lit hallway."
+    print "At the end of the hallway is a", count * 'long', 'staircase going towards some light'
     next = raw_input("> ")
     
     # infinite stairs option
     if next == "take stairs":
-        print 'You take the stairs'
+        print user + ' takes the stairs'
         if (count > 0):
-            print "but you're not happy about it"
-        infinite_stairway_room(count + 1)
-    # option 2 == ?????
-    if next == option_2:
-        pass
+            print "but she's not happy about it"
+        infinite_stairway_room(user, count + 1)
+    if next == "teleport":
+        gold_room(user)
 
 
-def gold_room():
-    print "This room is full of gold.  How much do you take?"
-
+def gold_room(user):
+    print "This room is full of gold.  How much does " + user + " take?"
     next = raw_input("> ")
-    if "0" in next or "1" in next:
+    if "0" in next or "1" in next or "1" in next or "2" in next or "3" in next or "4" in next or "5" in next or "6" in next or "7" in next or "8" in next or "9" in next:
         how_much = int(next)
     else:
         dead("Man, learn to type a number.")
 
     if how_much < 50:
-        print "Nice, you're not greedy, you win!"
+        print "Nice, " + user + " is not greedy, you win!"
         exit(0)
     else:
-        dead("You greedy bastard!")
+        dead(user + "," + "you greedy bastard!")
 
 
-def bear_room():
+def bear_room(user):
     print "There is a bear here."
     print "The bear has a bunch of honey."
     print "The fat bear is in front of another door."
-    print "How are you going to move the bear?"
+    print "How is " + user + " going to move the bear?"
     bear_moved = False
 
     while True:
         next = raw_input("> ")
 
-        if next == "take honey":
-            dead("The bear looks at you then slaps your face off.")
-        elif next == "taunt bear" and not bear_moved:
-            print "The bear has moved from the door. You can go through it now."
+        if next == "take" or "honey":
+            dead("The bear looks at " + user + " then slaps their face off.")
+        elif next == "taunt" and not bear_moved:
+            print "The bear has moved from the door. " + user + " can go through it now."
             bear_moved = True
-        elif next == "taunt bear" and bear_moved:
-            dead("The bear gets pissed off and chews your leg off.")
-        elif next == "open door" and bear_moved:
-            gold_room()
+        elif next == "taunt" and bear_moved:
+            dead("The bear gets pissed off and chews " + user + "'s" " leg off.")
+        elif next == ("open" or "door") and bear_moved:
+            gold_room(user)
         else:
             print "I got no idea what that means."
 
 
-def cthulhu_room():
-    print "Here you see the great evil Cthulhu."
-    print "He, it, whatever stares at you and you go insane."
+def cthulhu_room(user):
+    print "Here" + user + " sees the great evil Cthulhu."
+    print "He, it, whatever stares at" + user + "and " + user + " goes insane."
     print "Do you flee for your life or eat your head?"
 
     next = raw_input("> ")
 
     if "flee" in next:
-        start()
+        main()
     elif "head" in next:
         dead("Well that was tasty!")
     else:
@@ -83,20 +80,23 @@ def dead(why):
 
 
 ##############################################################################
-def start():
+def main():
+    user = sys.argv[1]
     # START the TextAdventure game
-    print "You are in a dark room."
-    print "There is a door to your right and left."
+    print user + " is in a dark room."
+    print "There is a door to your right and left, and a set of stairs in front of you."
     print "Which one do you take?"
 
     next = raw_input("> ")
 
     if next == "left":
-        bear_room()
+        bear_room(user)
     elif next == "right":
-        cthulhu_room()
+        cthulhu_room(user)
+    elif next == "stairs":
+        infinite_stairway_room(user, count=0)
     else:
-        dead("You stumble around the room until you starve.")
+        dead(user + " stumbles around the room until they starve.")
 
 if __name__ == '__main__':
-    start()
+    main()
